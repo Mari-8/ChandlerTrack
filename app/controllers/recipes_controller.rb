28fa8fc74d1f3a4baa2138e108869c7d01b35fbf
Mari-8 <ryanaltimari8@gmail.com>
2021-04-01 18:23:10 -0500

    Added crud functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,45 @@ class RecipesController < ApplicationController
     def show 
         recipe = Recipe.find_by(id: params[:id])
         options = {
-            include: [:inventoryItems, :notes]
+            include: [:inventory_items, :notes]
         }
         render json: RecipeSerializer.new(recipe, options)
+    end 
+
+    def index 
+        recipes = Recipe.all
+        options = {
+            include: [:inventory_items, :notes]
+        }
+        render json: RecipeSerializer.new(recipes)
+    end 
+
+    def create 
+        recipe = Recipe.new(recipe_params) 
+
+        if recipe.save 
+            RecipeSerializer.new(recipe) 
+        else 
+            render json: {error: "There was a problem creating this recipe"}
+        end 
+    end 
+
+    def destroy 
+        recipe = Recipe.find(params[:id])
+        recipe.destroy
+        render json: {message: "Successfully deleted recipe"}
+    end 
+
+    def update 
+        recipe = Recipe.find(params[:id]) 
+        recipe.update(recipe_params) 
+        render json: {message: "Successfully updated recipe"}
+    end 
+
+    private 
+
+    def recipe_params 
+        params.require(:recipe).permit(:name, :description)
     end 
 
 end
