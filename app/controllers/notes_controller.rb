39be47a766bd3d21f47c2dc588f64471a909b6d5
Mari-8 <ryanaltimari8@gmail.com>
2021-04-01 18:22:47 -0500

    Added crud functionality

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,40 @@
 class NotesController < ApplicationController
+    
+    def show 
+        note = Note.find_by(id: params[:id])
+        render json: NoteSerializer.new(note) 
+    end 
+
+    def index 
+        notes = Note.all 
+        render json: NoteSerializer.new(notes) 
+    end 
+
+    def create 
+        note = Note.new(note_params) 
+
+        if note.save 
+            render json: NoteSerializer.new(note)
+        else 
+            render json: {error: "There was a problem creating this note"}
+        end 
+    end 
+
+    def destroy 
+        note = Note.find(params[:id])
+        note.destroy 
+        render json: {message: "Successfully deleted note"}
+    end 
+
+    def update 
+        note = Note.find(params[:id]) 
+        note.update(note_params) 
+        render json: {message: "Successfully updated note"}
+    end 
+
+    private 
+
+    def note_params 
+        params.require(:note).permit(:note, :description, :amount)
+    end 
 end
