8e4f2f3ebf8bcda2d414a0e5edf09b7c532fd914
Mari-8 <ryanaltimari8@gmail.com>
2021-04-01 18:22:20 -0500

    Added crud functionality

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -3,8 +3,45 @@ class InventoryItemsController < ApplicationController
     def show 
         inventoryItem = InventoryItem.find_by(id: params[:id])
         options = {
-            include: [:recipes]
+            include: [:recipes, :ingredients]
         }
         render json: InventoryItemSerializer.new(inventoryItem, options)
+    end 
+
+    def index 
+        inventoryItems = InventoryItem.all 
+        options = {
+            include: [:recipes, :ingredients]
+        }
+        render json: InventoryItemSerializer.new(inventoryItems)
+    end 
+
+    def create 
+        inventoryItem = InventoryItem.new(inventoryItem_params)
+
+        if inventoryItem.save 
+           render json: InventoryItemSerializer.new(inventoryItem) 
+        else 
+            render json: {error: "There was a problem creating this item"}
+        end 
+    end 
+
+    def destroy 
+        inventoryItem = InventoryItem.find(params[:id])
+        inventoryItem.destroy
+        render json: {message: "Succesfully deleted item"}
+    end 
+
+    def update 
+        inventoryItem = InventoryItem.find(params[:id]) 
+        inventoryItem.update(inventoryItem_params) 
+        render json: {message: "Succesfully updated item"}
+    end 
+
+
+    private 
+
+    def inventoryItem_params
+        params.require(:inventory_item).permit(:name, :description, :amount)
     end 
 end
