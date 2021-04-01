ff4b96a5994e3f39d6cd69ca11d5d5559e99928e
Mari-8 <ryanaltimari8@gmail.com>
2021-04-01 18:21:57 -0500

    Added crud funtionality

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,48 @@
+class IngredientsController < ApplicationController
+
+    def index 
+        ingredients = Ingredient.all 
+
+        options = {
+            include: [:recipe, :inventory_item]
+        }
+        render json: IngredientSerializer.new(ingredients, options)
+    end 
+
+    def show 
+        ingredient = Ingredient.find_by(id: params[:id])
+
+        options = {
+            include: [:recipe, :inventory_item]
+        }
+        render json: IngredientSerializer.new(ingredient, options)
+    end 
+
+    def create 
+        ingredient = Ingredient.new(ingredient_params)
+
+        if ingredient.save 
+            render json: IngredientSerializer.new(ingredient) 
+        else 
+            render json: {error: "Could not create ingredient"} 
+        end 
+    end 
+
+    def destroy 
+        ingredient = Ingredient.find(params[:id])
+        ingredient.destroy 
+        render json: {message: "Succesfully deleted ingredient"}
+    end 
+
+    def update 
+        ingredient = Ingredient.find(params[:id])
+        ingredient.update(ingredient_params)
+        render json: {message: "Updated ingredient"}
+    end 
+
+    private 
+
+    def ingredient_params 
+        params.require(:ingredient).permit(:recipe_id, :inventory_item_id)
+    end 
+end
