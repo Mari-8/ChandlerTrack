bae7d6a1ecbda49bf929c0d33e23241af8a11edc
Mari-8 <ryanaltimari8@gmail.com>
2021-04-18 19:40:44 -0500

    Added event listeners for notes and ingredient delete buttons

--- frontend/src/recipe.js
+++ frontend/src/recipe.js
@@ -4,7 +4,6 @@ class Recipe{
     constructor(name, description, id) {
         this.id = id
         this.name = name 
-        this.description = description 
         this.element = document.createElement('div')
         this.element.id = `recipe-${this.id}`
 
@@ -40,13 +39,12 @@ class Recipe{
         this.element.innerHTML = `
         <div class="rec-container">
             <div class="item rec-info">
-                <strong class="name">${this.name}</strong>:
-                <span class="description">${this.description}</span>
+                <p class="box-text">${this.name}</p><br><br>
             </div>
             <div class="recipe-buttons" id="item rec-buttons">
-                <button class="edit" data-id="${this.id}">Edit</button>
-                <button class="delete" data-id="${this.id}">Delete</button>
-                <button class="view" id="view-recipe" data-id="${this.id}">View</button>
+            <button class="view" id="view-recipe" data-id="${this.id}">View</button>
+                <button class="edit" data-id="${this.id}">Edit</button><br>
+                <button class="delete" data-id="${this.id}">Delete</button><br>
             </div>
         </div>
         <br>
@@ -68,9 +66,7 @@ class Recipe{
         <h3>Add New Recipe</h3>
         <label for="recipe-name">Name:</label>
         <input type="text" name="name" id="recipe-name"><br><br>
-        <label for="recipe-description">Description:</label>
-        <input type="text" name="description" id="recipe-description"><br><br>
-        <input type="submit" value="Create">
+        <input class="button" type="submit" value="Create">
         </form>
         `
         recList.textContent = ''
@@ -79,15 +75,14 @@ class Recipe{
     }
 
     renderEditForm(recId) {
+        const recipe = Recipe.findById(recId)
         const recList = document.getElementById('rec-list')
         const editRecipeForm = document.createElement('div')
         editRecipeForm.innerHTML = `
-        <form id="edit-recipe-form" onsubmit="event.preventDefault();">
+        <form id="edit-recipe-form">
         <h3>Edit Recipe</h3>
         <label for="recipe-name">Name:</label>
-        <input type="text" name="name" id="recipe-name"><br><br>
-        <label for="recipe-description">Description:</label>
-        <input type="text" name="description" id="recipe-description"><br><br>
+        <input type="text" name="name" id="recipe-name" value="${recipe.name}"><br><br>
         <input type="submit" value="Update">
         </form>
         `
@@ -105,14 +100,35 @@ class Recipe{
         const ingredientList = document.getElementById("recipe-ingredient-list")
         let newLi = document.createElement('li')
         let ingredient = Ingredient.findById(ingr.id)
+        const deleteButton = document.getElementsByClassName('ing-delete-btn')
         let divElement = ingredient.element
         divElement.innerHTML = `
-        <h5>${ingr.name}</h5>
-        <h5>${ingr.amount} oz</h5>
+        <div id="ing-${ingr.id}" class="ingredient-display-box">
+            <h5> ${ingr.name} - ${ingr.amount} ${ingr.measurement} </h5>
+            <div class="outer">
+                 <div class="inner">
+                   <label class="ing-delete-btn" id="${ingr.id}">Delete</label>
+                </div>
+            </div>
+        </div>
         `
         newLi.appendChild(divElement)
         ingredientList.appendChild(newLi)
     }
+    
+    addIngListener() {
+        let ingredientsAdapter = new IngredientsAdapter
+        let deleteIngButtons = document.querySelectorAll('.ing-delete-btn') 
+        deleteIngButtons.forEach((btn) => { btn.addEventListener("click", (e) => { ingredientsAdapter.deleteIng(e.target.id); }); });
+    }
+
+    addNoteListener() {
+        let notesAdapter = new NotesAdapter 
+        let deleteNoteButtons = document.querySelectorAll('.note-delete-btn')
+        deleteNoteButtons.forEach((btn) => { btn.addEventListener("click", (e) => { notesAdapter.deleteNote(e.target.id); }); });
+    }
+
+
 
     addIngredients() {
         let ingredientsAdapter = new IngredientsAdapter
@@ -123,10 +139,18 @@ class Recipe{
     }
 
     addNoteToDom(note) {
-        const recNotesDiv = document.getElementsByClassName('recipe-notes')[0]
+        const recNotesDiv = document.getElementsByClassName('recipe-notes-list')[0]
         let divElement = note.element
         divElement.innerHTML = `
-        <h5>${note.note_body}<h5>
+        <div id="note-${note.id}" class="note-display-box"> 
+            <h5 class="note-body">${note.note_body}<h5>
+            <div class="outer-2">
+                <div class="inner-2">
+                    <label class="note-delete-btn" id="${note.id}">Delete</label>
+                </div>
+            </div>
+            </div>
+        <p class="push-left">_______________________</p>
         `
 
         recNotesDiv.appendChild(divElement)
@@ -140,11 +164,10 @@ class Recipe{
         }
     }
 
-   
 
-  
     viewRecipe(id) {
         const recId = id
+        const recipe = Recipe.findById(recId)
         const ingredient = new Ingredient
         const note = new Note
         const mainHeader = document.getElementById('main-head')
@@ -155,11 +178,13 @@ class Recipe{
         const newNoteButton = document.getElementById('new-note-button-container')
         recipeInfo.className = "recipe-info"
         recNotes.className = "recipe-notes" 
-        const recipe = Recipe.findById(recId)
         mainBody.innerText = ''
-        mainHeader.innerHTML = `<h1>${recipe.name}</h1>`
+        mainHeader.innerHTML = `
+        <h2>${recipe.name}</h2>
+        `
+
         recipeInfo.innerHTML = `
-            <h2>Ingredients</h2>
+            <h1>Ingredients</h1>
             <br>
             <p id="recipe-id" class="recipe-id" hidden="true">${recId}</p>
             <ul class="recipe-ingredient-list" id="recipe-ingredient-list">
@@ -170,7 +195,7 @@ class Recipe{
 
         recNotes.innerHTML = `
    
-            <h2>Notes</h2>
+            <h1>Notes</h1>
             <br>
             <ul class="recipe-notes-list" id="recipe-notes-list">
 
@@ -190,6 +215,9 @@ class Recipe{
         mainBody.appendChild(recNotes)
         recipe.addIngredients() 
         recipe.addNotes()
+        setTimeout(function(){ recipe.addNoteListener(); }, 500)
+        setTimeout(function(){ recipe.addIngListener(); }, 500);
+        
 
     }
  
