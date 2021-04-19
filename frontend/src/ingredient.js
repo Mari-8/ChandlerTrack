dbefba59ef761a01ee371a9c3cecbd36060317c7
Mari-8 <ryanaltimari8@gmail.com>
2021-04-18 19:35:33 -0500

    Added select option to new ingredient form

--- frontend/src/ingredient.js
+++ frontend/src/ingredient.js
@@ -1,9 +1,10 @@
 class Ingredient{
     static all = [] 
 
-    constructor(id, name, recipe_id, amount) {
+    constructor(id, name, recipe_id, amount, measurement) {
         this.id = id
         this.name = name 
+        this.measurement = measurement
         this.recipe_id = recipe_id 
         this.amount = amount
         this.element = document.createElement('div')
@@ -33,8 +34,14 @@ class Ingredient{
         <h3>Add ingredient</h3>
         <label for="ingredient-name">Name:</label>
         <input type="text" name="name" id="ingredient-name"><br><br>
-        <label for="ingredient-amount">Amount: (in ounces) </label> 
+        <label for="ingredient-amount">Amount: </label> 
         <input type="number" name="amount" id="ingredient-amount"><br><br>
+        <label for="ingredient-measurement">Measurement:</label> 
+        <select for="ingredient-measurement" id="ingredient-measurement"> 
+            <option>lbs</option>
+            <option>oz</option>
+            <option>units</option> 
+        </select><br><br>
         <input type="submit" id="new-ingr-button" value="Add">
         </form>
         `
@@ -42,8 +49,8 @@ class Ingredient{
         mainBody.appendChild(recipeInfo)
         const ingrButton = document.getElementById('new-ingr-button')
         ingrButton.addEventListener("click", function(e) {
-            e.preventDefault()
-            ingredient.handleAddIngredient(recId)
+             e.preventDefault()
+             ingredient.handleAddIngredient(recId)
         })
     }
 
@@ -51,12 +58,14 @@ class Ingredient{
         const recipe = Recipe.all.find(rec => rec.id == recId)
         const name = document.getElementById('ingredient-name').value
         const amount = document.getElementById('ingredient-amount').value
+        const measurement = document.getElementById('ingredient-measurement').value
         const ingredientsAdapter = new IngredientsAdapter
 
         let ingrObject = {
             name: name,
             recipe_id: recipe.id,
-            amount: amount
+            amount: amount,
+            measurement: measurement
         }
 
         ingredientsAdapter.addIngredient(ingrObject)
