ee93068d22053e9ea1c8c433950f2a04a8751e7b
Mari-8 <ryanaltimari8@gmail.com>
2021-04-18 19:43:45 -0500

    Added new consts for inv adapter, and new item button

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,15 @@
 const recipesAdapter = new RecipesAdapter
 const recipe = new Recipe
+const inventoryItem = new InventoryItem
+const inventoryItemsAdapter = new InventoryItemsAdapter
 const newRecipeButton = document.getElementById('add-recipe-button')
+const newInventoryItemButton = document.getElementById("add-item-button")
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    inventoryItemsAdapter.fetchInventoryItems()
     recipesAdapter.fetchRecipes()
     newRecipeButton.addEventListener("click", recipe.newRecipeForm)
+    newInventoryItemButton.addEventListener("click", inventoryItem.newItemForm)
     
     
 })
