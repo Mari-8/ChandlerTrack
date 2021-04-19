2cf79d7a2c519b0eea8e03b80fd28c14980bed72
Mari-8 <ryanaltimari8@gmail.com>
2021-04-18 19:29:23 -0500

    Added resources for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, :inventoryitems
-
+  resources :recipes 
+  resources :inventory_items
+  resources :ingredients
+  resources :notes
 end
